7c7b371f8800a0ea3db384184b95a39a1236eb05
carraes <carraeshb@gmail.com>
2022-12-19 12:20:15 -0300

    build: options

---
+++ lua/core/options.lua
@@ -0,0 +1,39 @@
+local opt = vim.opt 
+
+-- Line numbers
+opt.relativenumber = true
+opt.number = true
+
+-- Tab & indents
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+
+-- Wrapping
+opt.wrap = false
+
+-- Search
+opt.ignorecase = true
+opt.smartcase = true
+
+-- Cursor
+opt.cursorline = true
+
+-- Appearence
+opt.termguicolors = true
+opt.background = 'dark'
+opt.signcolumn = 'yes'
+
+-- Backspace
+opt.backspace = 'indent,eol,start'
+
+-- Clipboard
+opt.clipboard:append('unnamedplus')
+
+-- Split windows
+opt.splitright = true
+opt.splitbelow = true
+
+opt.iskeyword:append("-")
+
